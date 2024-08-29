e8d06f42cd8568d77c0811331dff4d8f13e2f067
Goh Jet Wei <goh_jetwei@mymail.sutd.edu.sg>
2024-08-29 23:34:46 +0800

    Update (prototype)_karaoke_singer_registration_streamlit_and_csv.py

--- (prototype, using csv file as database) Streamlit (Python Framework) ROOTech Karaoke Night project/(prototype)_karaoke_singer_registration_streamlit_and_csv.py
+++ (prototype, using csv file as database) Streamlit (Python Framework) ROOTech Karaoke Night project/(prototype)_karaoke_singer_registration_streamlit_and_csv.py
@@ -1,3 +1,7 @@
+#################
+#INCOMPLETE FILE#
+#################
+
 #Idea of this Streamlit (Python) Web Application Karaoke Event is to create 3 seperate files:
 #1. First is a streamlit app to get user input for their song choice and to indicate interest to add to queue 
 #   ('(main)_karaoke_singer_registration_streamlit_and_csv.py')
@@ -9,12 +13,10 @@
 #//////////////////////////////////////////////////////////////////////////////
 
 
-#This is app '3. Third is the streamlit app that displays the queue on the karaoke projector screen in a 
-#graphing/figure format'
+#This is app '1. First is a streamlit app to get user input for their song choice and to indicate interest to add 
+#to queue'
 import streamlit as st
 import pandas as pd
-import random
-from queue_data_structure_class import Queue
 from karaoke_singer_class import KaraokeSinger
 
 
@@ -23,38 +25,17 @@ from karaoke_singer_class import KaraokeSinger
 
 #BACKEND - With csv file and Pythonic commands side of things...
 
-#Using ChatGPT
-def get_random_element(my_list):
-    """
-    Returns a random element from the input list.
-    
-    Args:
-    my_list (list): The list from which to choose a random element.
-    
-    Returns:
-    A random element from the input list.
-    """
-    if not my_list:
-        return None  # Return None if the list is empty
-    else:
-        return random.choice(my_list)
-
-# # Example usage:
-# my_list = [1, 2, 3, 4, 5]
-# random_element = get_random_element(my_list)
-# print("Random Element:", random_element)
-
 database = pd.read_csv("database.csv")
 print(database)
 
-queue = Queue()
+database_array = []
 
-#Loading the csv file contents into the Queue Data Structure, enqueueing the person at the top of the database first 
-#(to show that he will be the next singer)
+#Loading the csv file contents into an Array, to make manipulation of the database easier (such as when you want to
+#append a new 'KaraokeSinger')
 for index, row in database.iterrows():
     user = KaraokeSinger(row['name'], row['song_choice'])
-    queue.enqueue(user)
-print(queue)
+    database_array.append(user)
+print(database_array)
 
 
 #/////////////////////////////////////////////////////////////////////////
@@ -62,19 +43,73 @@ print(queue)
 
 #FRONTEND - Streamlit (Python) app side of things...
 
-st.markdown("<h1 style='text-align: center;'> 🔥🔥Song Queue🔥🔥 </h1>", unsafe_allow_html=True)
-st.markdown("---")
+st.image(image="karaoke_poster.jpg", width=680)
+st.header("🎶 Add Your Name and Your Song Choice! 🎶")
 
 
-music_emojis = ["🎵", "🎶", "🎤", "🎧", "🎸", "🥁", "🎺", "🎷", "🎻", "🎹", "🎼", "🎤🎸", "🎶🕺", "🕺💃", "🎵🎤", "🎤🎵", "🎤🎸", "🎷🎶", "🎹🎵", "🎶🎵", "🎵🔊", "🎚️"]
-column1, column2 = st.columns(2)
+#The double confirmation functionality to prompt users to do a confirmation before submitting their inputs to 
+# minimise false inputs:
+# name1 = st.text_input(label="Name (or your stage name!)", placeholder="Singer Name")
+# song_choice1 = st.text_input(label="Song choice (please enter the song name and artist so we can find the right song for you)", placeholder="Song Name(Artist)")
 
-with column1:
-    st.write("### *Next Singer🎤*")
-    for i in reversed(queue.buffer):
-        st.write(i.name, get_random_element(music_emojis))
+# button_state = st.button(label="Submit")
 
-with column2:
-    st.write("### *Song choice🎶*")
-    for i in reversed(queue.buffer):
-        st.write(i.song_choice)
+# if button_state:
+#     if name1 == "" or song_choice1 == "":
+#         st.warning("Please fill up all the above fields!")
+#     else:
+#         with st.form(key="Form 1"):
+#             st.write("Confirm?")
+#             st.write("Name: ", name1)
+#             st.write("Song choice: ", song_choice1)
+
+#             submit_button_state = st.form_submit_button(label="Confirm Submit")
+#             if submit_button_state:
+#                 st.success("Submitted successfully!")
+
+#                 #Uploading the 'KaraokeSinger''s name and song choice into the 'database_array'
+#                 database_array.append(KaraokeSinger(name1, song_choice1))
+#                 print(database_array)
+
+with st.form(key="Form 1"):
+    name = st.text_input(label="Name (or your stage name!)")
+    song_choice = st.text_input(label="Song choice (please enter the song name and artist so we can find the right song for you)")
+
+    submit_button_state = st.form_submit_button(label="Submit")
+
+    if submit_button_state:
+        if name == "" or song_choice == "":
+            st.warning("Please fill up all the above fields!")
+        else:
+            st.write("Confirm?")
+            st.write("Name: ", name)
+            st.write("Song choice: ", song_choice)
+
+
+####################
+#UNFIXED ERROR HERE#
+####################
+            # submit_button_state2 = st.form_submit_button(label="Submit")
+            if submit_button_state:
+                st.success("Submitted successfully!")
+
+                #Uploading the 'KaraokeSinger''s name and song choice into the 'database_array'
+                database_array.append(KaraokeSinger(name, song_choice))
+                print(database_array)
+
+
+#/////////////////////////////////////////////////////////////////////////
+
+
+#BACKEND - With csv file and Pythonic commands side of things...
+
+#Reupdating the database from 'database_array' to 'updated_database'
+
+#Converting each 'KaraokeSinger' object in the 'database_array' into a dictionary, then appending them back into the 
+#'updated_database' DataFrame
+updated_database = pd.DataFrame([x.as_dict() for x in database_array])
+print(updated_database)
+updated_database.to_csv('database.csv', index=False)
+
+
+
